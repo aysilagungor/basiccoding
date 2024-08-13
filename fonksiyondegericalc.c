fd37bf77055afd1ffa379f0001bd57688d72dd5d
Aysıla Güngör <gungoraysila@gmail.com>
2024-08-13 22:30:48 +0300

    Update fonksiyondegericalc.c

--- fonksiyondegericalc.c
+++ fonksiyondegericalc.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-float x,y,fxy;
-printf("x değerini giriniz -> "); scanf("%f", &x);printf("y değerini giriniz -> "); scanf("%f", &y);
+float x,y,fxy; char deger=1;
+printf("x degerini giriniz -> "); scanf("%f", &x);printf("y değerini giriniz -> "); scanf("%f", &y);
 
 
 if (x>=0)
 if(y>=0)
-        {fxy = x*y; printf("x=%.2f ve y=%.2f oldugunda fonk. degeri= %.2f olur.\n\n",x,y,fxy);}
+        fxy = x*y;
   else
 
-        {fxy=x+y; printf("x=%.2f ve y=%.2f oldugunda fonk. degeri= %.2f olur.\n\n",x,y,fxy);}
+        fxy=x+y;
     else
-    if(y>=0) {
-    fxy = y-x; printf("x=%.2f ve y=%.2f oldugunda fonk. degeri= %.2f olur.\n\n",x,y,fxy);}
+    if(y>=0)
+    fxy = y-x;
 
+    else
+        deger=0;
+     if (deger==1)
+
+         printf("x=%.2f ve y=%.2f oldugunda fonk. degeri %2.f olur",x,y,fxy);
     else
      printf("sartlar saglanmadigi icin fonk degeri bulunamadi.\n\n");
 
-    return 0;
+return 0;
 }
